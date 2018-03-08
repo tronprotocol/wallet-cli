9e7d0066be98e39db6aa2d163e1eec9e5f4c2389
yangchang <yangchang@tronlab.com>
2018-03-08 22:08:34 +0800

    add explorer package and method

--- src/main/java/org/tron/explorer/controller/GrpcClientController.java
+++ src/main/java/org/tron/explorer/controller/GrpcClientController.java
@@ -5,6 +5,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,9 +28,9 @@ public class GrpcClientController {
     return new ModelAndView("index");
   }
 
-  @RequestMapping("/balance")
-    public String getBalance(@RequestParam(defaultValue =
-      "4154ca3d1de87d61ab9f96891b6b2c359d6e8a94") String address) {
+  // 4154ca3d1de87d61ab9f96891b6b2c359d6e8a94
+  @RequestMapping("/balance/{address}")
+    public String getBalance(@PathVariable(value ="address") String address) {
     Account account = Account.newBuilder().setAddress(ByteString
         .copyFrom(ByteArray.fromHexString(address))).build();
     return grpcClientService.getBalance(account).toString();
