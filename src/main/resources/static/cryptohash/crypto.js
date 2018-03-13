71bea81efc9cbf106520de8adb2f81463ec7780f
zergweak <zhang_anwen@163.com>
2018-03-13 22:28:19 +0800

    add js function ECKeySig SHA256

--- src/main/resources/static/cryptohash/crypto.js
+++ src/main/resources/static/cryptohash/crypto.js
@@ -1,4 +1,5 @@
 
+//return address by HexString, pubBytes is byte[]
 function computeAddress (pubBytes) {
   var pubKey = bin2String(pubBytes);
   if (pubKey.length == 65){
@@ -6,4 +7,19 @@ function computeAddress (pubBytes) {
   }
   var hash = CryptoJS.SHA3(pubKey).toString();
   return hash.substring(24);
+}
+
+//return sign by 65 bytes
+function ECKeySign(hashBytes, priKeyBytes) {
+  var EC = elliptic.ec;
+  var ec = new EC('secp256k1');
+  var key = ec.keyFromPrivate(priKeyBytes, 'hex');
+  var signature = key.sign(hashBytes);
+  return signature;
+}
+
+//toDO:
+//return 32 bytes
+function SHA256(msgBytes) {
+  return  hexStr2byteArray("3031323334353630313233343536373839");
 }
