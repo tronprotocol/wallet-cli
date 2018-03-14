386c4d2c89f3ee9ad4b822b473c6cf1a14b04083
zergweak <zhang_anwen@163.com>
2018-03-14 12:23:13 +0800

    implementation function ECKeySign of js

--- src/main/resources/static/cryptohash/crypto.js
+++ src/main/resources/static/cryptohash/crypto.js
@@ -9,7 +9,7 @@ function computeAddress (pubBytes) {
   return hash.substring(24);
 }
 
-//return sign by 65 bytes
+//return sign by 65 bytes r s id. id < 27
 function ECKeySign(hashBytes, priKeyBytes) {
   var EC = elliptic.ec;
   var ec = new EC('secp256k1');
@@ -18,16 +18,27 @@ function ECKeySign(hashBytes, priKeyBytes) {
   var r = signature.r;
   var s = signature.s;
   var id = signature.recoveryParam;
-  var rStr = r.toString('hex');
-  var sStr = s.toString('hex');
-  var idStr = byte2hexStr(id);
-  var signStr = rStr+sStr+idStr;
-  var signByts = hexStr2byteArray(signStr);
-  return signByts;
+  var rHex = r.toString('hex');
+  while (rHex.length < 64 ){
+    rHex = "00" + rHex;
+  }
+  var sHex = s.toString('hex');
+  while (sHex.length < 64 ){
+    sHex = "00" + sHex;
+  }
+  var idHex = byte2hexStr(id);
+  var signHex = rHex+sHex+idHex;
+  var signBytes = hexStr2byteArray(signHex);
+  return signBytes;
 }
 
 //toDO:
 //return 32 bytes
 function SHA256(msgBytes) {
-  return  hexStr2byteArray("3031323334353630313233343536373839");
+  var shaObj = new jsSHA("SHA-256", "HEX");
+  var msgHex = byteArray2hexStr(msgBytes);
+  shaObj.update(msgHex);
+  var hashHex = shaObj.getHash("HEX");
+  var hashBytes = hexStr2byteArray(hashHex);
+  return hashBytes;
 }
