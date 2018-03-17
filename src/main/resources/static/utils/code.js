fe021307266a7b153948b05e2856404f84085b91
zergweak <zhang_anwen@163.com>
2018-03-17 21:57:22 +0800

    add git commit -m add

--- src/main/resources/static/utils/code.js
+++ src/main/resources/static/utils/code.js
@@ -1,24 +1,127 @@
-
 function bin2String(array) {
   return String.fromCharCode.apply(String, array);
+}
+
+function arrayEquals(array1, array2) {
+  if (array1.length != array2.length){
+    return false;
+  }
+  var i;
+  for(i = 0; i < array1.length; i++){
+    if (array1[i] != array2[i]){
+      return false;
+    }
+  }
+  return true;
+}
+
+function getTransActionFromBase64String(base64String) {
+  var base64Bytes = stringToBytes(base64String);
+  var bytesDecode = base64Decode(base64Bytes);
+  var transaction = proto.protocol.Transaction.deserializeBinary(bytesDecode);
+  //ToDo : ret is success
+  return transaction;
+}
+
+//Return a list contains contract object
+function getContractListFromTransaction(transaction) {
+  var raw = transaction.getRawData();
+  var type = raw.getType();
+  if (type != 1) {
+    alert("Invalid transaction type !!!!" + type);
+    return null;
+  }
+  var contractList = raw.getContractList();
+  var count = contractList.length;
+  if (count == 0) {
+    alert("No contract !!!!");
+    return null;
+  }
+
+  array = new Array(count);
+  var unpack = proto.google.protobuf.Any.prototype.unpack;
+  while (count > 0) {
+    count--;
+    var oneContract = contractList[count];
+    var any = oneContract.getParameter();
+    var contarcType = oneContract.getType();
+    var obje;
+    switch (contarcType) {
+      case proto.protocol.Transaction.Contract.ContractType.ACCOUNTCREATECONTRACT:
+        obje = any.unpack(
+            proto.protocol.AccountCreateContract.deserializeBinary,
+            "protocol.AccountCreateContract");
+        break;
+
+      case proto.protocol.Transaction.Contract.ContractType.TRANSFERCONTRACT:
+        obje = any.unpack(
+            proto.protocol.TransferContract.deserializeBinary,
+            "protocol.TransferContract");
+        break;
+
+      case proto.protocol.Transaction.Contract.ContractType.TRANSFERASSERTCONTRACT:
+        obje = any.unpack(
+            proto.protocol.TransferAssertContract.deserializeBinary,
+            "protocol.TransferAssertContract");
+        break;
+
+      case proto.protocol.Transaction.Contract.ContractType.VOTEASSETCONTRACT:
+        obje = any.unpack(
+            proto.protocol.VoteAssetContract.deserializeBinary,
+            "protocol.VoteAssetContract");
+        break;
+
+      case proto.protocol.Transaction.Contract.ContractType.VOTEWITNESSCONTRACT:
+        obje = any.unpack(
+            proto.protocol.VoteWitnessContract.deserializeBinary,
+            "protocol.VoteWitnessContract");
+        break;
+
+      case proto.protocol.Transaction.Contract.ContractType.WITNESSCREATECONTRACT:
+        obje = any.unpack(
+            proto.protocol.WitnessCreateContract.deserializeBinary,
+            "protocol.WitnessCreateContract");
+        break;
+
+      case proto.protocol.Transaction.Contract.ContractType.ASSETISSUECONTRACT:
+        obje = any.unpack(
+            proto.protocol.AssetIssueContract.deserializeBinary,
+            "protocol.AssetIssueContract");
+        break;
+
+      case proto.protocol.Transaction.Contract.ContractType.DEPLOYCONTRACT:
+        obje = any.unpack(
+            proto.protocol.DeployContract.deserializeBinary,
+            "protocol.DeployContract");
+        break;
+
+      case proto.protocol.Transaction.Contract.ContractType.WITNESSUPDATECONTRACT:
+        obje = any.unpack(
+            proto.protocol.WitnessUpdateContract.deserializeBinary,
+            "protocol.WitnessUpdateContract");
+        break;
+    }
+    array[count] = obje;
+  }
+  return array;
 }
 
 function stringToBytes(str) {
   var bytes = new Array();
   var len, c;
   len = str.length;
-  for(var i = 0; i < len; i++) {
+  for (var i = 0; i < len; i++) {
     c = str.charCodeAt(i);
-    if(c >= 0x010000 && c <= 0x10FFFF) {
+    if (c >= 0x010000 && c <= 0x10FFFF) {
       bytes.push(((c >> 18) & 0x07) | 0xF0);
       bytes.push(((c >> 12) & 0x3F) | 0x80);
       bytes.push(((c >> 6) & 0x3F) | 0x80);
       bytes.push((c & 0x3F) | 0x80);
-    } else if(c >= 0x000800 && c <= 0x00FFFF) {
+    } else if (c >= 0x000800 && c <= 0x00FFFF) {
       bytes.push(((c >> 12) & 0x0F) | 0xE0);
       bytes.push(((c >> 6) & 0x3F) | 0x80);
       bytes.push((c & 0x3F) | 0x80);
-    } else if(c >= 0x000080 && c <= 0x0007FF) {
+    } else if (c >= 0x000080 && c <= 0x0007FF) {
       bytes.push(((c >> 6) & 0x1F) | 0xC0);
       bytes.push((c & 0x3F) | 0x80);
     } else {
@@ -27,23 +130,21 @@ function stringToBytes(str) {
   }
   return bytes;
 
-
 }
 
-
 function bytesToString(arr) {
-  if(typeof arr === 'string') {
+  if (typeof arr === 'string') {
     return arr;
   }
   var str = '',
       _arr = arr;
-  for(var i = 0; i < _arr.length; i++) {
+  for (var i = 0; i < _arr.length; i++) {
     var one = _arr[i].toString(2),
         v = one.match(/^1+?(?=0)/);
-    if(v && one.length == 8) {
+    if (v && one.length == 8) {
       var bytesLength = v[0].length;
       var store = _arr[i].toString(2).slice(7 - bytesLength);
-      for(var st = 1; st < bytesLength; st++) {
+      for (var st = 1; st < bytesLength; st++) {
         store += _arr[st + i].toString(2).slice(2);
       }
       str += String.fromCharCode(parseInt(store, 2));
@@ -54,7 +155,6 @@ function bytesToString(arr) {
   }
   return str;
 }
-
 
 function hextoString(hex) {
   var arr = hex.split("")
@@ -68,58 +168,45 @@ function hextoString(hex) {
 }
 
 /* Convert a hex char to value */
-function hexChar2byte(c)
-{
+function hexChar2byte(c) {
   var d = 0;
-  if (c>='A' && c<='F')
-  {
+  if (c >= 'A' && c <= 'F') {
     d = c.charCodeAt(0) - 'A'.charCodeAt(0) + 10;
   }
-  else if (c>='a' && c<='f')
-  {
+  else if (c >= 'a' && c <= 'f') {
     d = c.charCodeAt(0) - 'a'.charCodeAt(0) + 10;
   }
-  else if (c>='0' && c<='9')
-  {
+  else if (c >= '0' && c <= '9') {
     d = c.charCodeAt(0) - '0'.charCodeAt(0);
   }
   return d;
 }
 
-
 /* Check if a char is hex char */
-function isHexChar(c)
-{
-  if ((c>='A' && c<='F') ||
-      (c>='a' && c<='f') ||
-      (c>='0' && c<='9'))
-  {
+function isHexChar(c) {
+  if ((c >= 'A' && c <= 'F') ||
+      (c >= 'a' && c <= 'f') ||
+      (c >= '0' && c <= '9')) {
     return 1;
   }
   return 0;
 }
 
-
 /* Convert HEX string to byte array */
-function hexStr2byteArray(str)
-{
+function hexStr2byteArray(str) {
   var byteArray = Array();
   var d = 0;
   var i = 0;
   var j = 0;
   var k = 0;
 
-
-  for (i=0; i<str.length; i++)
-  {
+  for (i = 0; i < str.length; i++) {
     var c = str.charAt(i);
-    if (isHexChar(c))
-    {
+    if (isHexChar(c)) {
       d <<= 4;
       d += hexChar2byte(c);
       j++;
-      if (0==(j%2))
-      {
+      if (0 == (j % 2)) {
         byteArray[k++] = d;
         d = 0;
       }
@@ -128,10 +215,8 @@ function hexStr2byteArray(str)
   return byteArray;
 }
 
-
 /* Convert a byte to string */
-function byte2hexStr(byte)
-{
+function byte2hexStr(byte) {
   var hexByteMap = "0123456789ABCDEF";
   var str = "";
   str += hexByteMap.charAt(byte >> 4);
@@ -139,28 +224,26 @@ function byte2hexStr(byte)
   return str;
 }
 
-
 /* Convert byte arry to HEX string */
-function byteArray2hexStr(byteArray)
-{
+function byteArray2hexStr(byteArray) {
   var str = "";
-  for (var i=0; i<(byteArray.length-1); i++)
-  {
+  for (var i = 0; i < (byteArray.length - 1); i++) {
     str += byte2hexStr(byteArray[i]);
   }
   str += byte2hexStr(byteArray[i]);
   return str;
 }
 
-function  base64Decode(bytes64) {
+function base64Decode(bytes64) {
   var string64 = bytesToString(bytes64);
   var b = new Base64();
   var decodeBytes = b.decodeToByteArray(string64);
 //  var decodeBytes = stringToBytes(decodeString);
   return decodeBytes;
 }
+
 //return baset64 bytes
-function  base64Encode(bytes) {
+function base64Encode(bytes) {
   var string = bytesToString(bytes);
   var b = new Base64();
   var string64 = b.encode(string);
@@ -258,7 +341,7 @@ function Base64() {
     var byteArray = new Array(utftext.length)
     var i = 0;
     var c = c1 = c2 = 0;
-    while ( i < utftext.length ) {
+    while (i < utftext.length) {
       c = utftext.charCodeAt(i);
       byteArray[i] = c;
       i++;
@@ -268,13 +351,13 @@ function Base64() {
 
   // private method for UTF-8 encoding
   _utf8_encode = function (string) {
-    string = string.replace(/\r\n/g,"\n");
+    string = string.replace(/\r\n/g, "\n");
     var utftext = "";
     for (var n = 0; n < string.length; n++) {
       var c = string.charCodeAt(n);
       if (c < 128) {
         utftext += String.fromCharCode(c);
-      } else if((c > 127) && (c < 2048)) {
+      } else if ((c > 127) && (c < 2048)) {
         utftext += String.fromCharCode((c >> 6) | 192);
         utftext += String.fromCharCode((c & 63) | 128);
       } else {
@@ -292,19 +375,20 @@ function Base64() {
     var string = "";
     var i = 0;
     var c = c1 = c2 = 0;
-    while ( i < utftext.length ) {
+    while (i < utftext.length) {
       c = utftext.charCodeAt(i);
       if (c < 128) {
         string += String.fromCharCode(c);
         i++;
-      } else if((c > 191) && (c < 224)) {
-        c2 = utftext.charCodeAt(i+1);
+      } else if ((c > 191) && (c < 224)) {
+        c2 = utftext.charCodeAt(i + 1);
         string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
         i += 2;
       } else {
-        c2 = utftext.charCodeAt(i+1);
-        c3 = utftext.charCodeAt(i+2);
-        string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+        c2 = utftext.charCodeAt(i + 1);
+        c3 = utftext.charCodeAt(i + 2);
+        string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3
+            & 63));
         i += 3;
       }
     }
