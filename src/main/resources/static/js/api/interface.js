0f0bc8c477e36783b76ff8191054f4f2b0642696
徐文杰 <431027130@qq.com>
2018-03-22 16:01:36 +0800

    解决冲突

--- src/main/resources/static/js/api/interface.js
+++ src/main/resources/static/js/api/interface.js
@@ -1,11 +1,10 @@
 
-//var baseUrl = 'http://192.168.10.131:8088/';
+//var baseUrl = 'http://localhost:8088/';
 
-var baseUrl = 'http://localhost:8088/';
+var baseUrl = 'http://192.168.10.195:8088/';
 
-// var create =http://192.168.10.131:8088/sendCoinToView;
 var trans = baseUrl+'sendCoinToView'; //转账接口
 var anintran = baseUrl+'transactionFromView'; // 转账接口
 var witelist = baseUrl +'witnessList' //见证人接口
 
-
+var accountList = baseUrl +'accountList' //查询账户列表
