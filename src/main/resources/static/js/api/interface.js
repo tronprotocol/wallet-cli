3ba0decb30f32668705f7c4e7c7d259e9cd35da1
徐文杰 <431027130@qq.com>
2018-03-22 20:11:57 +0800

    witness

--- src/main/resources/static/js/api/interface.js
+++ src/main/resources/static/js/api/interface.js
@@ -8,5 +8,7 @@ var anintran = baseUrl+'transactionFromView'; // 转账接口
 var witelist = baseUrl +'witnessList' //见证人接口
 
 var accountList = baseUrl +'accountList' //查询账户列表
+var witnessList = baseUrl +'witnessList' //查询见证人列表
 
 var getBlockToView = baseUrl +'getBlockToView' //查询账户列表
+
