4e60c6f0ca0cef79285aa53126ea5e905949f198
renchengchang <rencc@sina.com>
2018-03-22 11:15:15 +0800

    adjust dir

--- src/main/resources/web/js/utils/control.js
+++
@@ -1,7 +0,0 @@
-$(".c_form li").each(function(i,item){
-    $(this).on('click',function () {
-        $(this).addClass('c_active').siblings().removeClass('c_active');
-        $('.c_table>div').eq(i).show().siblings().hide();
-
-    })
-});
