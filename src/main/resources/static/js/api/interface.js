9e4dedfa9ef4dbb4df087b492f6c7dd3dce16ba6
yangchang <yangchang@tronlab.com>
2018-03-22 17:17:52 +0800

    remove useless method and javascript

--- src/main/resources/static/js/api/interface.js
+++ src/main/resources/static/js/api/interface.js
@@ -8,3 +8,5 @@ var anintran = baseUrl+'transactionFromView'; // 转账接口
 var witelist = baseUrl +'witnessList' //见证人接口
 
 var accountList = baseUrl +'accountList' //查询账户列表
+
+var getBlockToView = baseUrl +'getBlockToView' //查询账户列表
