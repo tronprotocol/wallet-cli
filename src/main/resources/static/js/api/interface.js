52ab9b02a6300fd0ab49956a3e09c101ca0ccba6
yangchang <yangchang@tronlab.com>
2018-03-23 11:58:29 +0800

    remove useless method and javascript

--- src/main/resources/static/js/api/interface.js
+++ src/main/resources/static/js/api/interface.js
@@ -9,7 +9,7 @@ var getAccountInfo = baseUrl +'queryAccount'; //查询账户详情
 var accountList = baseUrl +'accountList' //查询账户列表
 var witnessList = baseUrl +'witnessList' //查询见证人列表
 var getBlockToView = baseUrl +'getBlockToView' //current block
-var getBlockByNumToView = baseUrl +'getBlockToView' //recent block
+var getBlockByNumToView = baseUrl +'getBlockByNumToView' //recent block
 
 var createAssetView = baseUrl + 'createAssetIssueToView' //发行资产
 var signView = baseUrl + 'transactionFromView'; //签名接口
