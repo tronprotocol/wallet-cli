98e46a9c2c436d8ea6b7bfcc70d2b69fdc52b01f
徐文杰 <431027130@qq.com>
2018-03-24 22:11:45 +0800

    style

--- src/main/resources/static/js/utils/block.js
+++ src/main/resources/static/js/utils/block.js
@@ -1,6 +1,21 @@
 
 
-
+// $(function() {
+//     var listPanel = $('#recentHtml');
+//     var z = 0; //向上滚动top值
+//     function up() { //向上滚动
+//         listPanel.animate({ //中奖结果
+//             'top': (z - 35) + 'px'
+//         }, 1500, 'slow', function() {
+//             listPanel.css({
+//                 'top': '0px'
+//             })
+//                 .find("li:first").appendTo(listPanel).animate({opacity:1});;
+//             up();
+//         });
+//     }
+//     up();
+// });
 
 TransSuccessCallback = function (data) {
 
@@ -16,6 +31,7 @@ TransSuccessCallback = function (data) {
   var witnessNum=1
   parenthash = blockData.getBlockHeader().getRawData().getParenthash();
   parenthashHex = byteArray2hexStr(parenthash);
+  parenthashHexSix = parenthashHex.substr(0,6) + '...'
   var contraxtType=proto.protocol.Transaction.Contract.ContractType;
 
   var contractType;
@@ -27,14 +43,14 @@ TransSuccessCallback = function (data) {
   // time
   var time =10;
   var str="";
-  function getTx(contractName,ownerHex,amount,toHex,time) {
-    str += '<p class="transfer">'
+  function getTx(contractName,ownerHex,amount,toHex) {
+    str += '<li class="transfer">'
         + '<button >'+contractName+'</button>'
         + '<span class="tran_name">' + ownerHex + '</span>'
         + '<span>将' + amount + ' TRX转帐给</span>'
         + '<span class="tran_name">' + toHex + '</span>'
-        + '<span>' + time + '秒钟前</span>'
-        + '</p>';
+        // + '<span>' + time + '秒钟前</span>'
+        + '</li>';
 
     $("#recentHtml").html(str);
 
@@ -44,9 +60,9 @@ TransSuccessCallback = function (data) {
 
   $("#block_num").text(blockNumber);
   $("#witness_num").text(witnessNum);
-  $("#beforeBlock").text(parenthashHex);
+  $("#beforeBlock").text(parenthashHexSix);
 
-  console.log("parenthashHex : " + parenthashHex);
+ // console.log("parenthashHex : " + parenthashHex);
 
   var witnessNum = 1;
 
@@ -55,8 +71,8 @@ TransSuccessCallback = function (data) {
   var txlist = blockData.getTransactionsList();
 
   if (txlist.length > 0) {
-
-    for (var index in txlist) {
+    var txlistFive = txlist.slice(0,10)
+    for (var index in txlistFive) {
       // console.log(txlist[index]);
       var tx = txlist[index];
       contractList = tx.getRawData().getContractList();
@@ -88,17 +104,20 @@ TransSuccessCallback = function (data) {
 
             var owner = obje.getOwnerAddress();
              ownerHex = byteArray2hexStr(owner);
+             ownerHexSix = ownerHex.substr(0,6) + '...';
 
             var to = obje.getToAddress();
              toHex = byteArray2hexStr(to);
 
+             toHexSix = toHex.substr(0,6) + '...';
+
              amount = obje.getAmount();
 
-            console.log("ownerHex " + ownerHex);
-            console.log("to  " + toHex);
-            console.log("amount  " + amount);
+            // console.log("ownerHex " + ownerHex);
+            // console.log("to  " + toHex);
+            // console.log("amount  " + amount);
 
-            getTx(contractName,ownerHex,amount,toHex,time);
+            getTx(contractName,ownerHexSix,amount,toHexSix);
 
             break;
 
@@ -107,14 +126,10 @@ TransSuccessCallback = function (data) {
 
 
 
-
-
-
-    // get before block
-     ajaxRequest("GET", getBlockByNumToView, {num: blockNumber - 1},
-          TransSuccessCallback, TransFailureCallback);
-
-    }
+  }
+      // get before block
+      ajaxRequest("GET", getBlockByNumToView, {num: blockNumber - 1},
+          TransSuccessByNumToViewCallback, TransFailureCallback);
 
   }
 };
@@ -129,31 +144,35 @@ TransFailureCallback = function (err) {
 // query current block
 ajaxRequest( "GET",getBlockToView,{},TransSuccessCallback,TransFailureCallback);
 
-
+setInterval(function () {
+    ajaxRequest( "GET",getBlockToView,{},TransSuccessCallback,TransFailureCallback);
+},10000)
 
 //query recent block
 
-TransSuccessCallback = function (data) {
+TransSuccessByNumToViewCallback = function (data) {
   var recentBlock = base64DecodeFromString(data);
 
   var blockData = proto.protocol.Block.deserializeBinary(recentBlock);
   var blockNumber= blockData.getBlockHeader().getRawData().getNumber();
   var witnessAddress= blockData.getBlockHeader().getRawData().getWitnessAddress();
   var witnessAddressHex=byteArray2hexStr(witnessAddress);
+  var witnessAddressHexSix = witnessAddressHex.substr(0,10) + '...';
   var time= blockData.getBlockHeader().getRawData().getTimestamp();
   var transactionNum= blockData.getTransactionsList().length;
   var contraxtType=proto.protocol.Transaction.Contract.ContractType;
   var big = 255;
-
+  var newDate = new Date(time);
+  var minutes = newDate.getMinutes();
  // console.log(blockNumber+" ::: "+time+" ::: "+witnessAddressHex+" ::: "+transactionNum);
 
 
   var html= '<div  class="mr_left">'
       + '<p>区块  #'+ blockNumber+'</p>'
-      + '<p>'+time+'前</p>'
+      + '<p>'+minutes+'分前</p>'
       + ' </div>'
       + '<div class="mr_right">'
-      + '<p>见证人'+witnessAddressHex+'  </p><p>'
+      + '<p>见证人: '+witnessAddressHexSix+'  </p><p>'
       + '<span>交易数：'+transactionNum+'</span>'
       +'<span>大小：'+big+'</span></p></div>';
 
