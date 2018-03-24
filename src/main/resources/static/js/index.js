743afe0db0d5dca4d712fc24ff1ac43d7cfd916d
徐文杰 <431027130@qq.com>
2018-03-24 18:34:14 +0800

    ..status

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -31,13 +31,13 @@ $(document).ready(function(){
     $('#money').click(function () {
         $(this).addClass('header_active').siblings().removeClass('header_active');
         $('#text').load('html/transform.html');
-        $('#text').css('background','url(img/bg.png)')
+        $('#text').css('background','url(../static/img/bg.png)')
 
     });
     $('#create').click(function () {
         $('#text').load('html/login.html');
         $('.header span').removeClass('header_active');
-        $('#text').css('background','url(img/bg.png)')
+        $('#text').css('background','url(../static/img/bg.png)')
     });
     $('#nemoney').click(function () {
         $('#text').css('background','none');
