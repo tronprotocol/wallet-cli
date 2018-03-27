06235ce3a2f9ab5c1372531f4509bdeb3cd496d9
yangchang <yangchang@tronlab.com>
2018-03-27 17:21:19 +0800

    add explorer test class

---
+++ src/test/java/org/tron/explorer/controller/VoteWitnessControllerTest.java
@@ -0,0 +1,16 @@
+package org.tron.explorer.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class VoteWitnessControllerTest {
+
+  @Test
+  public void getVoteWitnessList() {
+  }
+
+  @Test
+  public void getTransactionToView() {
+  }
+}
