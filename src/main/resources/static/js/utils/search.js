8bb806024a3e50660c662a379f66b9bcfeabcbb7
renchengchang <rencc@sina.com>
2018-03-28 15:18:28 +0800

    search block

--- src/main/resources/static/js/utils/search.js
+++ src/main/resources/static/js/utils/search.js
@@ -1,7 +1,6 @@
 
 
-var addr  = window.localStorage.getItem('address');
-
+var addr = $('#searchInput').val();
 
 if(getStringType(addr) == 1) {
     searchAccount(addr);
@@ -11,31 +10,48 @@ if(getStringType(addr) == 3) {
 }
 
 function searchBlock(height){
-
+    ajaxRequest( "GET",getBlockToView,{},TransSuccessCallback,TransFailureCallback);
 }
 
 
-searchAssetSuccess = function (data) {
-    var curTime = new Date().getTime();
-    var content = "";
-    var assetIssueContract = proto.protocol.AssetIssueContract.deserializeBinary(base64DecodeFromString(data));
-    var name = bytesToString(assetIssueContract.getName());
-    alert(name)
-    var ownerAddress = byteArray2hexStr(assetIssueContract.getOwnerAddress());
-    var totalSupply = assetIssueContract.getTotalSupply();
-    var startTime = assetIssueContract.getStartTime();
-    var endTime = assetIssueContract.getEndTime();
-    var formattedStartTime = formateDate(startTime);
-    var formattedEndTime = formateDate(endTime);
-    if(!(startTime < curTime && curTime< endTime)){
-        content += "<tr><td>" + name + "</td><td>" + ownerAddress + "</td><td>" + totalSupply + "</td> <td class='stop'>1</td><td><input type='button' class='add_account time_end' value='参与'/></td></tr>";
-    }else{
-        content += "<tr><td>" + name + "</td><td>" + ownerAddress + "</td><td>" + totalSupply + "</td> <td > " + formattedStartTime + " - " + formattedEndTime + " </td><td><input type='button' class='add_account' value='参与' onclick=\"participateAssetIssue(" + i + ")\"/></td></tr>";
+function formateDate(timeStamp) {
+    var dateObj = new Date(timeStamp);
+    var year=dateObj.getFullYear();
+    var month=dateObj.getMonth() + 1;
+    if(month<10) month = "0" + month;
+    var date=dateObj.getDate();
+    if(date<10) date = "0" + date;
+    var hour=dateObj.getHours();
+    if(hour<10) hour = "0" + hour;
+    var minute=dateObj.getMinutes();
+    if(minute<10) minute = "0" + minute;
+    var second=dateObj.getSeconds();
+    if(second<10) second = "0" + second;
+    return year+"-"+month+"-"+date+" "+hour+":"+minute+":"+second;
+}
+
+function searchAssetSuccess(data) {
+    if(data) {
+        var curTime = new Date().getTime();
+        var content = "";
+        var assetIssueContract = proto.protocol.AssetIssueContract.deserializeBinary(base64DecodeFromString(data));
+        var name = bytesToString(assetIssueContract.getName());
+        var ownerAddress = byteArray2hexStr(assetIssueContract.getOwnerAddress());
+        var totalSupply = assetIssueContract.getTotalSupply();
+        var startTime = assetIssueContract.getStartTime();
+        var endTime = assetIssueContract.getEndTime();
+        var formattedStartTime = formateDate(startTime);
+        var formattedEndTime = formateDate(endTime);
+        if (!(startTime < curTime && curTime < endTime)) {
+            content += "<tr><td>" + name + "</td><td>" + ownerAddress + "</td><td>" + totalSupply + "</td> <td class='stop'>1</td><td><input type='button' class='add_account time_end' value='参与'/></td></tr>";
+        } else {
+            content += "<tr><td>" + name + "</td><td>" + ownerAddress + "</td><td>" + totalSupply + "</td> <td > " + formattedStartTime + " - " + formattedEndTime + " </td><td><input type='button' class='add_account' value='参与' onclick=\"participateAssetIssue(" + i + ")\"/></td></tr>";
+        }
+        $('#searchAssetResult').append(content);
     }
-    $('#searchAssetResult').append(content);
 }
 
-searchAssetFailure = function (data) {
+function searchAssetFailure(data) {
     layer.alert("获取资产列表失败");
 }
 
