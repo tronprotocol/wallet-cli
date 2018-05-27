666134483f4ab604a0e3c862398ff70923cb47a8
zergweak <zhang_anwen@163.com>
2018-05-27 14:54:10 +0800

    print time 2 long

--- src/main/java/org/tron/common/utils/Utils.java
+++ src/main/java/org/tron/common/utils/Utils.java
@@ -168,7 +168,7 @@ public class Utils {
         result += account.getAssetMap().get(name);
         result += "\n";
         result += "  latest_asset_operation_time: ";
-        result += new Date(account.getLatestAssetOperationTimeMap().get(name));
+        result += account.getLatestAssetOperationTimeMap().get(name);
         result += "\n";
         result += "  free_asset_net_usage: ";
         result += account.getFreeAssetNetUsageMap().get(name);
@@ -198,11 +198,11 @@ public class Utils {
     result += "\n";
 
     result += "latest_consume_time: ";
-    result += new Date(account.getLatestConsumeTime());
+    result += account.getLatestConsumeTime();
     result += "\n";
 
     result += "latest_consume_free_time: ";
-    result += new Date(account.getLatestConsumeFreeTime());
+    result += account.getLatestConsumeFreeTime();
     result += "\n";
 
     result += "allowance: ";
