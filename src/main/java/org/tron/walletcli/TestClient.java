d8f81f3e508bb7e07f7b25f58b1becf252c79106
ashu <ithinker1991@gmail.com>
2018-08-03 11:53:23 +0800

    print StackTrace

--- src/main/java/org/tron/walletcli/TestClient.java
+++ src/main/java/org/tron/walletcli/TestClient.java
@@ -1787,12 +1787,11 @@ public class TestClient {
       } catch (CancelException e) {
         System.out.println(cmd + " failed!");
         System.out.println(e.getMessage());
+      } catch (Exception e) {
+        System.out.println(cmd + " failed!");
+        logger.error(e.getMessage());
+        logger.error(e.fillInStackTrace());
       }
-
-//      catch (Exception e) {
-//        System.out.println(cmd + " failed!");
-//        logger.error(e.getMessage());
-//      }
     }
   }
 
