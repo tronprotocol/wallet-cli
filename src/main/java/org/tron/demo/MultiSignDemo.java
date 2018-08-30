df6ed85faa6d0a21d3d597fdcf5a8eff9b621441
zergweak <zhang_anwen@163.com>
2018-08-30 18:52:30 +0800

    add test

--- src/main/java/org/tron/demo/MultiSignDemo.java
+++ src/main/java/org/tron/demo/MultiSignDemo.java
@@ -17,17 +17,27 @@ public class MultiSignDemo {
     String owner = "TJCnKsPa7y5okkXvQAidZBzqx3QyQ6sxMW";
     String private0 = "D95611A9AF2A2A45359106222ED1AFED48853D9A44DEFF8DC7913F5CBA727366";
     String private1 = "cba92a516ea09f620a16ff7ee95ce0df1d56550a8babe9964981a7144c8a784a";
+    String private2 = "8E812436A0E3323166E1F0E8BA79E19E217B2C4A53C970D4CCA0CFB1078979DF";
     long amount = 10_000_000_000L;
-    Transaction transaction = TransactionSignDemo.createTransaction(WalletApi.decodeFromBase58Check(owner),
-        WalletApi.decodeFromBase58Check(to), amount);
-    TransactionExtention transactionExtention = WalletApi.addSignByApi(transaction, ByteArray.fromHexString(private0));
-   // System.out.println(Utils.printTransaction(transactionExtention));
-    TransactionSignWeight transactionSignWeight = WalletApi.getTransactionSignWeight(transactionExtention.getTransaction());
+    Transaction transaction = TransactionSignDemo
+        .createTransaction(WalletApi.decodeFromBase58Check(owner),
+            WalletApi.decodeFromBase58Check(to), amount);
+    TransactionExtention transactionExtention = WalletApi
+        .addSignByApi(transaction, ByteArray.fromHexString(private0));
+    // System.out.println(Utils.printTransaction(transactionExtention));
+    TransactionSignWeight transactionSignWeight = WalletApi
+        .getTransactionSignWeight(transactionExtention.getTransaction());
     System.out.println(Utils.printTransactionSignWeight(transactionSignWeight));
 
-    transactionExtention = WalletApi.addSignByApi(transactionExtention.getTransaction(), ByteArray.fromHexString(private1 ));
-   // System.out.println(Utils.printTransaction(transactionExtention));
-    transactionSignWeight = WalletApi.getTransactionSignWeight(transactionExtention.getTransaction());
+    transactionExtention = WalletApi
+        .addSignByApi(transactionExtention.getTransaction(), ByteArray.fromHexString(private1));
+    // System.out.println(Utils.printTransaction(transactionExtention));
+    transactionSignWeight = WalletApi
+        .getTransactionSignWeight(transactionExtention.getTransaction());
     System.out.println(Utils.printTransactionSignWeight(transactionSignWeight));
+
+    transactionExtention = WalletApi
+        .addSignByApi(transactionExtention.getTransaction(), ByteArray.fromHexString(private2));
+    System.out.println(Utils.printTransaction(transactionExtention));
   }
 }
