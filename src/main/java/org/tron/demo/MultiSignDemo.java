373f90e2fbe08e798d7737bdc56f59fcf3c69cdf
aljokerhacker <72671616+aljokerhacker@users.noreply.github.com>
2022-08-16 19:12:53 +0300

    Update MultiSignDemo.java

--- src/main/java/org/tron/demo/MultiSignDemo.java
+++ src/main/java/org/tron/demo/MultiSignDemo.java
@@ -13,7 +13,7 @@ public class MultiSignDemo {
   public static void main(String[] args) throws CancelException {
     String to = "TL5mpGbtr5L2Gi7CtotBQzjN8pK7SmbyFz";
     String owner = "TJCnKsPa7y5okkXvQAidZBzqx3QyQ6sxMW";
-    String private0 = "D95611A9AF2A2A45359106222ED1AFED48853D9A44DEFF8DC7913F5CBA727366";
+    String private0 = "7f484aa5655a058fc9076651d248c79b9cc78ce97c949c94083894bbc1e70c4e";
     String private1 = "cba92a516ea09f620a16ff7ee95ce0df1d56550a8babe9964981a7144c8a784a";
     String private2 = "8E812436A0E3323166E1F0E8BA79E19E217B2C4A53C970D4CCA0CFB1078979DF";
     long amount = 10_000_000_000L;
