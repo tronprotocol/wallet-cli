e9e071229ceec997153d29887e14a414ad267b99
jiangyuanshu <317787106@qq.com>
2024-10-08 14:51:03 +0800

    revert some changes

--- src/test/java/org/tron/keystore/StringUtilsTest.java
+++ src/test/java/org/tron/keystore/StringUtilsTest.java
@@ -29,16 +29,6 @@ public class StringUtilsTest {
     char[] b = "ghijkl".toCharArray();
     char[] c = "defghi".toCharArray();
     char[] d = "abcdefghijkl".toCharArray();
-    char[] empty = "".toCharArray();
-
-    char[] longarr = "xxxxxxxx123xxxxxxxxx".toCharArray();
-    char[] shortarr = "123".toCharArray();
-    Assert.assertFalse(StringUtils.isContains(shortarr, longarr));
-    Assert.assertTrue(StringUtils.isContains(longarr, shortarr));
-
-    Assert.assertFalse(StringUtils.isContains(empty, d));
-    Assert.assertFalse(StringUtils.isContains(d, empty));
-
     Assert.assertTrue(StringUtils.isContains(d, d));
     Assert.assertTrue(StringUtils.isContains(d, a));
     Assert.assertTrue(StringUtils.isContains(d, b));
