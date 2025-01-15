d5ec7b70bf399b529a385bdb9ce5308264327927
chaozhu <chaosuper85@gmail.com>
2025-01-15 16:52:41 +0800

    support trident

---
+++ src/test/java/org/tron/walletserver/WalletApiTest.java
@@ -0,0 +1,12 @@
+package org.tron.walletserver;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class WalletApiTest {
+
+  @Test
+  public void test001() {
+    Assert.assertTrue(true);
+  }
+}
