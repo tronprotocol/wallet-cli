106ae42ffb8efa441581b8b948cf19b24db0ae34
chaozhu <chaosuper85@gmail.com>
2025-01-15 16:15:32 +0800

    support mnemonic

---
+++ src/main/java/org/tron/mnemonic/MnemonicUtils.java
@@ -0,0 +1,114 @@
+package org.tron.mnemonic;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.bitcoinj.crypto.MnemonicCode;
+import org.tron.common.utils.ByteArray;
+import org.tron.core.exception.CipherException;
+import org.web3j.crypto.Bip32ECKeyPair;
+import org.web3j.crypto.Credentials;
+
+import java.io.File;
+import java.io.IOException;
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MnemonicUtils {
+  private static final String FilePath = "Mnemonic";
+  private static final ObjectMapper objectMapper = new ObjectMapper();
+
+  static {
+    objectMapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
+    objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+  }
+
+  public static List<String> generateMnemonic(SecureRandom secureRandom) {
+    List<String> words = null;
+    byte[] entropy = new byte[16];
+    secureRandom.nextBytes(entropy);
+    try {
+      words = MnemonicCode.INSTANCE.toMnemonic(entropy);
+    } catch (Exception e) {
+
+    }
+    return words;
+  }
+
+  public static String mnemonicWordsToString(List<String> mnemonicWords) {
+    if (mnemonicWords == null || mnemonicWords.isEmpty()) {
+      return "";
+    }
+    return String.join(" ", mnemonicWords);
+  }
+
+  public static List<String> stringToMnemonicWords(String mnemonicString) {
+    if (mnemonicString == null || mnemonicString.trim().isEmpty()) {
+      return new ArrayList<>();
+    }
+    return Arrays.asList(mnemonicString.trim().split("\\s+"));
+  }
+
+  public static String store2Keystore(MnemonicFile mnemonicFile) throws IOException {
+    if (mnemonicFile == null) {
+      System.out.println("Warning: Store mnemonic failed, mnemonicFile is null !!");
+      return null;
+    }
+    File file = new File(FilePath);
+    if (!file.exists()) {
+      if (!file.mkdir()) {
+        throw new IOException("Make directory failed!");
+      }
+    } else {
+      if (!file.isDirectory()) {
+        if (file.delete()) {
+          if (!file.mkdir()) {
+            throw new IOException("Make directory failed!");
+          }
+        } else {
+          throw new IOException("File exists and can not be deleted!");
+        }
+      }
+    }
+    return generateWalletFile(mnemonicFile, file);
+  }
+
+  public static String generateWalletFile(MnemonicFile walletFile, File destinationDirectory)
+      throws IOException {
+    String fileName = getWalletFileName(walletFile);
+    File destination = new File(destinationDirectory, fileName);
+
+    objectMapper.writeValue(destination, walletFile);
+    return fileName;
+  }
+
+  private static String getWalletFileName(MnemonicFile mnemonicFile) {
+    return mnemonicFile.getAddress() + ".json";
+  }
+
+  public static byte[] exportMnemonic(byte[] password, String ownerAddress) throws IOException, CipherException {
+    File file = new File("Mnemonic/" + ownerAddress + ".json");
+    if (!file.exists()) {
+      System.out.println("mnemonic file of the address: " + ownerAddress + " not exist");
+      System.out.println("Please use ImportWalletByMnemonic to import the wallet or RegisterWallet to create a new wallet.");
+    }
+    MnemonicFile mnemonicFile = objectMapper.readValue(file, MnemonicFile.class);
+    return Mnemonic.decrypt2MnemonicWordsBytes(password, mnemonicFile);
+  }
+
+  public static byte[] getPrivateKeyFromMnemonic(List<String> mnemonics) {
+    int HARDENED_BIT = 0x80000000;
+    String mnemonic = String.join(" ", mnemonics);
+    byte[] seed = org.web3j.crypto.MnemonicUtils.generateSeed(mnemonic, "");
+    Bip32ECKeyPair masterKeypair = Bip32ECKeyPair.generateKeyPair(seed);
+    // m/44'/195'/0'/0/0
+    final int[] path = {44 | HARDENED_BIT, 195 | HARDENED_BIT, 0 | HARDENED_BIT, 0, 0};
+    Bip32ECKeyPair bip44Keypair = Bip32ECKeyPair.deriveKeyPair(masterKeypair, path);
+    Credentials credentials = Credentials.create(bip44Keypair);
+    String privateKey = credentials.getEcKeyPair().getPrivateKey().toString(16);
+
+    return ByteArray.fromHexString(privateKey);
+  }
+}
