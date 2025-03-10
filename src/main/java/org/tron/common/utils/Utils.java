d1b34228accecab3d7ad6778d84c17fa88bb67bc
chaozhu <chaosuper85@gmail.com>
2025-03-10 17:43:22 +0800

    fix build.gradle and fix 2 issues

--- src/main/java/org/tron/common/utils/Utils.java
+++ src/main/java/org/tron/common/utils/Utils.java
@@ -290,25 +290,23 @@ public class Utils {
   public static char[] inputPasswordWithoutCheck() throws IOException {
     char[] password;
     Console cons = System.console();
-    while (true) {
-      if (cons != null) {
-        password = cons.readPassword("password: ");
-      } else {
-        byte[] passwd0 = new byte[64];
-        int len = System.in.read(passwd0, 0, passwd0.length);
-        int i;
-        for (i = 0; i < len; i++) {
-          if (passwd0[i] == 0x09 || passwd0[i] == 0x0A) {
-            break;
-          }
+    if (cons != null) {
+      password = cons.readPassword("password: ");
+    } else {
+      byte[] passwd0 = new byte[64];
+      int len = System.in.read(passwd0, 0, passwd0.length);
+      int i;
+      for (i = 0; i < len; i++) {
+        if (passwd0[i] == 0x09 || passwd0[i] == 0x0A) {
+          break;
         }
-        byte[] passwd1 = Arrays.copyOfRange(passwd0, 0, i);
-        password = StringUtils.byte2Char(passwd1);
-        StringUtils.clear(passwd0);
-        StringUtils.clear(passwd1);
       }
-      return password;
+      byte[] passwd1 = Arrays.copyOfRange(passwd0, 0, i);
+      password = StringUtils.byte2Char(passwd1);
+      StringUtils.clear(passwd0);
+      StringUtils.clear(passwd1);
     }
+    return password;
   }
 
   public static byte[] generateContractAddress(Transaction trx, byte[] ownerAddress) {
